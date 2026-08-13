c6145c4fd7b0c8cfe625cfc8b1e2083f926a3db1
Moisés Vitor <moisesdamaziojandreiche@gmail.com>
2026-08-13 11:32:21 -0300

    Finalização da tela de pedômetro

--- components/Movimento.js
+++ components/Movimento.js
@@ -3,9 +3,43 @@ import { useEffect, useState } from 'react';
 import { DeviceMotion } from 'expo-sensors';
 
 export default function Movimento() {
+
+  const [status, setStatus] = useState("Parado");
+
+  useEffect(() => {
+    let inscricao = null;
+
+    DeviceMotion.setUpdateInterval(200);
+    inscricao = DeviceMotion.addListener((monitor) => {
+      //Construir a lógica de detecção de movimento
+
+      const aceleracao = monitor.acceleration;
+
+      if (aceleracao == null) {
+        return;
+      }
+
+      if (Math.abs(aceleracao.x) > 5 ||
+          Math.abs(aceleracao.y) > 5 ||
+          Math.abs(aceleracao.z) > 5)
+      {
+        setStatus("Movimento detectado");
+      }
+      else
+      {
+        setStatus("Parado");
+      }
+
+      return () => {
+        inscricao?.remove();
+      }
+    });
+
+  }, []);
+
   return (
     <View>
-      <Text> Tela de movimento </Text>
+      <Text>{status}</Text>
     </View>
   );
 }
--- components/Pedometro.js
+++ components/Pedometro.js
@@ -1,9 +1,74 @@
-import { View, Text } from 'react-native';
+import { View, Text, Plataform, PermissionsAndorid } from 'react-native';
+import { Pedometer } from 'expo-sensors';
 
 export default function Pedometro() {
+ 
+  const [ disponivel, setDisponivel ] = useState(false);
+  const [ passosPassados, setPassosPassados ] = useState(0);
+  const [ passosAtuais, setPassosAtuais ] = useState(0);
+
+useEffect(() =>{
+  let inscricao = null
+
+  async function configutarPedometro() {
+    const estaDisponivel = await Pedometer.isvailableAsync();
+    setDisponivel(estaDisponivel)
+
+    const inicio = new Date();
+    const fim = new Date();
+    inicio.serDate(fim.getDAte() - 1);
+
+
+    let resultado;
+    if(Plataform.OS == 'ios') {
+      resultado = 
+        await Pedometer.getStepCountAsync(inicio, fim);
+
+        setPassosPassados(resultado.steps);
+
+        inscricao = Pedometer.watchStepCount((monitor) =>{
+            setPassosAtuais(monitor.steps);
+        });
+    }
+    else{
+      const autorizado = await PermissionsAndorid.request(
+        PermissionsAndorid.PERMISSIONS.ACTIVITY_RECOGNITION
+      );
+
+      if (autorizado === PermissionsAndorid.RESULTS.GRANTED){
+        inscricao = Pedometer.watchStepCount((monitor) =>{
+            setPassosAtuais(monitor.steps);
+      });
+    }
+  }
+}
+  configutarPedometro();
+
+
+  return () =>{
+    inscricao?.remove();
+  }
+}, []);
+
+function passosNoDia() {
+  if (Plataform.OS == "ios"){
+    return <Text>Passos hoje: {passosPassados}</Text>
+  }else{
+    return <Text>Sem contagem disponivel</Text>
+  }
+}
   return (
     <View>
       <Text> Tela de pedômetro </Text>
+      <View>
+        <Text>Sensor Disponivel:
+          {disponivel === true ? "Sim" : "Não" }
+        </Text>
+
+        {passosNoDia()}
+
+        <Text>Passos atuais: {passosAtuais}</Text>
+      </View>
     </View>
   );
 }
